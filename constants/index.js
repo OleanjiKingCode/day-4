9b1184040ff9ce9cdf7a9a55a29d507acd7e568b
OleanjiKingCode <holamilean56@gmail.com>
2022-04-08 11:39:54 +0100

    Sample Commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x5c047072963E2c770D890f5e0ea10a1E3Ad6B9d9";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
